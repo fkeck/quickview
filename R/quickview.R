bfd07ec90cb426dbdd262058da77a6b8724897c2
fkeck <francois.keck@gmail.com>
2021-05-10 19:39:58 +0200

    Improve print: colors and maximum length output

--- R/quickview.R
+++ R/quickview.R
@@ -34,22 +34,38 @@ quickview <- function() {
     block <- context$selection[[1]]$text
   }
 
+  if(getOption("quickview.print", default = "default") == "default") {
+    msg <- strsplit(block, split = "\n")[[1]]
+    if(length(msg) > 12) {
+      msg <- paste(msg[1:12], collapse = "\n")
+      msg <- paste(msg, " ...")
+    } else {
+      msg <- paste(msg, collapse = "\n")
+    }
+    msg <- paste0("\033[90m", msg, "\033[39m")
+    msg_header <- paste0("\033[40m\033[37m\033[1m",
+                         "            QuickView            ",
+                         "\033[22m\033[39m\033[49m")
+    msg <- paste("\n", msg_header, "\n\n", msg, "\n\n")
+  }
+  cat(msg)
+
   comm <- paste0('View(', block, ')')
-  cat("\n", block, "\n")
   eval(parse(text = comm))
 
 }
 
 
-
-# Find blocks of code in a character vector returned by rstudioapi::getActiveDocumentContext()$content
+# Find blocks of code in a character vector returned by
+# rstudioapi::getActiveDocumentContext()$content
 detect_com <- function(x){
+
   regex_op <- "(\\+|-|\\*|/|\\^|%|:|\\$|@|%|<|>|=|!|&|\\||~|,) *$"
   debt_par <- cumsum(sapply(x, stringr::str_count, pattern = "\\(") - sapply(x, stringr::str_count, pattern = "\\)"))
   debt_sqb <- cumsum(sapply(x, stringr::str_count, pattern = "\\[") - sapply(x, stringr::str_count, pattern = "\\]"))
 
   blocks <- vector("integer", length(x))
-  k <- 1
+  k <- 1L
   for(i in seq_along(blocks)){
     if(debt_par[i] != 0 || debt_sqb[i] != 0 || grepl(regex_op, x[i]) || grepl("^ *$", x[i]) ) {
       blocks[i] <- k
@@ -59,6 +75,7 @@ detect_com <- function(x){
     }
   }
   return(blocks)
+
 }
 
 
